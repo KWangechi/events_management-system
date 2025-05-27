70ee172f762e0d570f0a6a9b1308c04320647664
KWangechi <rachel.karanja@strathmore.edu>
2025-05-27 12:47:17 +0300

    refactor: remove prefix backslash in test route

--- api/routes/web.php
+++ api/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test', function () {
+Route::get('test', function () {
     // return view('welcome');
     return response()->json(['message' => 'Welcome to the Event Management API']);
 });
